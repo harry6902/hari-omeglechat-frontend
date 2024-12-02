335f22a39d643e9a9154545a0a5cf31eee14474e
harry6902 <161632926+harry6902@users.noreply.github.com>
2024-12-02 18:10:25 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env.NEXT_PUBLIC_BACKEND_URL': JSON.stringify(process.env.NEXT_PUBLIC_BACKEND_URL),
+    BACKEND_URL: JSON.stringify(process.env.VITE_BACKEND_URL),
   },
 })
